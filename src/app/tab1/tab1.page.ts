a1f36673fa11e6e08800486e94f1090bc1f7ad0b
Ravi Teja Darapu <ravievolution87@gmail.com>
2021-03-16 12:42:01 +0530

    updated ui components of home and album

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+import recentlyPlayed from '../../assets/mockdata/recentlyPlayed.json';
+import heavyRotation from '../../assets/mockdata/heavyRotation.json';
+import jumpBackIn from '../../assets/mockdata/jumpBackIn.json';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +12,40 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  data = [
+    {
+      title: 'Recently played',
+      albums: recentlyPlayed
+    },
+    {
+      title: 'Heavy rotation',
+      albums: heavyRotation
+    },
+    {
+      title: 'Jump back in',
+      albums: jumpBackIn
+    }
+  ];
+ 
+  opts = {
+    slidesPerView: 2.4,
+    slidesOffsetBefore: 20,
+    spaceBetween: 20,
+    freeMode: true
+  };
+
+  constructor(private router: Router) {}
+
+  openAlbum(album) {
+    const titleEscaped = encodeURIComponent(album.title);
+    this.router.navigateByUrl(`/tabs/tab1/${titleEscaped}`);
+  }
+ 
+  // Helper function for image names
+  dasherize(string) {
+    return string.replace(/[A-Z]/g, function(char, index) {
+      return (index !== 0 ? '-' : '') + char.toLowerCase();
+    });
+  };
 
 }
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonTabs } from '@ionic/angular';
 
 @Component({
   selector: 'app-tabs',
@@ -7,6 +8,14 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
+  @ViewChild('tabs') tabs: IonTabs;
+  selected = '';
+  progress=42;
+
   constructor() {}
+
+  setSelectedTab(){
+    this.selected = this.tabs.getSelected();
+  }
 
 }
